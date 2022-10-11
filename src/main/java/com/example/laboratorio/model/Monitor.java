fed2288ce5b79f073c3a3811fc2b54cdcc1b3106
Cristhian C <ceballoscristhian4@gmail.com>
2022-10-11 10:32:30 -0500

    Changes by Derly EQP

--- src/main/java/com/example/laboratorio/model/Monitor.java
+++ src/main/java/com/example/laboratorio/model/Monitor.java
@@ -1,4 +1,28 @@
 package com.example.laboratorio.model;
 
 public class Monitor extends Person{
+    private String career;
+    private int semester;
+
+    public Monitor(String name, String id, String career, int semester) {
+        super(name, id);
+        this.career = career;
+        this.semester = semester;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
 }
--- src/main/java/com/example/laboratorio/model/Teacher.java
+++ src/main/java/com/example/laboratorio/model/Teacher.java
@@ -1,4 +1,38 @@
 package com.example.laboratorio.model;
 
 public class Teacher extends Person{
+    private int yearsOfAntiguaty;
+    private String career;
+    private String asignature;
+
+    public Teacher(String name, String id, int yearsOfAntiguaty, String career, String asignature) {
+        super(name, id);
+        this.yearsOfAntiguaty = yearsOfAntiguaty;
+        this.career = career;
+        this.asignature = asignature;
+    }
+
+    public int getYearsOfAntiguaty() {
+        return yearsOfAntiguaty;
+    }
+
+    public void setYearsOfAntiguaty(int yearsOfAntiguaty) {
+        this.yearsOfAntiguaty = yearsOfAntiguaty;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public String getAsignature() {
+        return asignature;
+    }
+
+    public void setAsignature(String asignature) {
+        this.asignature = asignature;
+    }
 }
