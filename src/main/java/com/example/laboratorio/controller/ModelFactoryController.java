41485be081dd3f500a155c171c72fb096c4ec541
Cristhian C <ceballoscristhian4@gmail.com>
2022-10-11 10:32:36 -0500

    Changes by Estudiante29

--- src/main/java/com/example/laboratorio/controller/ModelFactoryController.java
+++ src/main/java/com/example/laboratorio/controller/ModelFactoryController.java
@@ -1,6 +1,23 @@
 package com.example.laboratorio.controller;
 
 import com.example.laboratorio.services.ModelFactoryControllerService;
+import com.example.laboratorio.services.impl.LaboratoryServiceImpl;
 
 public class ModelFactoryController implements ModelFactoryControllerService {
+    LaboratoryServiceImpl laboratory;
+    private static class SingletonHolder {
+        // El constructor de Singleton puede ser llamado desde aquí al ser protected
+        private final static ModelFactoryController eINSTANCE = new ModelFactoryController();
+    }
+
+    // Método para obtener la instancia de nuestra clase
+    public static ModelFactoryController getInstance() {
+        return SingletonHolder.eINSTANCE;
+    }
+
+    public ModelFactoryController() {
+        System.out.println("invocación clase singleton");
+        laboratory = new LaboratoryServiceImpl();
+    }
+
 }
